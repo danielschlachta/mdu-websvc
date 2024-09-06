75ac49ed352853591632766ba72a003858a6aa79
Daniel Schlachta <daniel.schlachta@gmail.com>
2024-09-06 17:15:55 +0300

    Fixed minor issues with php8 (no more implicit float->int), had NetBeans auto-format all files.

--- common.php
+++ common.php
@@ -1,102 +1,97 @@
 <?php
-    $sim_table_name = 'sims';
-    $slot_table_name = 'slots';
-	$history_table_name = 'history';
-	$ext_table_name = 'external';
-	
-	date_default_timezone_set($timezone);
 
-    function error($text, $conn) {
-            echo $text;
-            
-            if ($conn)
-                    echo ": " . mysqli_error($conn);
-            exit;
-    } 
+$sim_table_name = 'sims';
+$slot_table_name = 'slots';
+$history_table_name = 'history';
+$ext_table_name = 'external';
 
-    $connect = @mysqli_connect($db_host, $db_user, $db_pass);
+date_default_timezone_set($timezone);
 
-    if (!$connect)
-    {
-        echo 'Unable to connect to mysql';
-        exit;
-    }
-    
-    if (!@mysqli_select_db($connect, $db_name))
-        error('Unable to select database', $connect);
-        
-        
-   	$time = time();
-	
-	$intsel=@$_POST['interval'];
-	
-	$yearsel=@$_POST['year'];
-	$monthsel=@$_POST['month'];
-	$daysel=@$_POST['day'];
-	
-	$year = date('Y', $time);
-	$month = date('n', $time);
-	$day = date('j', $time);
+function error($text, $conn) {
+    echo $text;
 
-	$months = array(
-		1 => 'January',
-		2 => 'February',
-		3 => 'March',
-		4 => 'April',
-		5 => 'May',
-		6 => 'June',
-		7 => 'July',
-		8 => 'August',
-		9 => 'September',
-		10 => 'October',
-		11 => 'November',
-		12 => 'December'
-	);
-	
-	$intervals = array (
-		1 => "the day",
-		2 => "the week",
-		3 => "the month",
-		4 => "the year"
-	);
-	
-	$intsel = $intsel == '' ? '1' : $intsel;
-	$yearsel = $yearsel == '' ? $year : $yearsel;
-	$monthsel = $monthsel == '' ? $month : $monthsel;
-	$daysel = $daysel == '' ? $day : $daysel;
-		
-	function dateref($interval, $year, $month, $day) {
-		return '<a href="' . "?page=graph&amp;interval=$interval&amp;year=$year&amp;month=$month&amp;day=$day" . '">';
-	}	
-	
-	function timeref($interval, $time) {
-		$year = date('Y', $time);
-		$month = date('n', $time);
-		$day = date('j', $time);
-		
-		return dateref($interval, $year, $month, $day);
-	}	
-	
-	$starttime = mktime(0, 0, 0, $monthsel, $daysel, $yearsel);
-	$endtime = $starttime + 86400; 
-	$cal_caption = date('l, F j Y', $starttime);
-	
-	if ($intsel == 2)
-	{
-		$starttime = strtotime('this week', $starttime);
-		$endtime = $starttime + 86400 * 7;
-		$cal_caption = 'Week of ' . date('F j, Y', $starttime);
+    if ($conn)
+        echo ": " . mysqli_error($conn);
+    exit;
+}
 
-	} else if ($intsel == 3)
-	{
-		$starttime = strtotime('first day of this month', $starttime);
-		$endtime = strtotime('first day of next month', $starttime);
-		$cal_caption = date('F Y', $starttime);
-	} else if ($intsel == 4)
-	{
-		$starttime = strtotime('first day of january', $starttime);
-		$endtime = strtotime('last day of december', $starttime);
-		$cal_caption = date('Y', $starttime);
-	}
+$connect = @mysqli_connect($db_host, $db_user, $db_pass);
 
+if (!$connect) {
+    echo 'Unable to connect to mysql';
+    exit;
+}
+
+if (!@mysqli_select_db($connect, $db_name))
+    error('Unable to select database', $connect);
+
+
+$time = time();
+
+$intsel = @$_POST['interval'];
+
+$yearsel = @$_POST['year'];
+$monthsel = @$_POST['month'];
+$daysel = @$_POST['day'];
+
+$year = date('Y', $time);
+$month = date('n', $time);
+$day = date('j', $time);
+
+$months = array(
+    1 => 'January',
+    2 => 'February',
+    3 => 'March',
+    4 => 'April',
+    5 => 'May',
+    6 => 'June',
+    7 => 'July',
+    8 => 'August',
+    9 => 'September',
+    10 => 'October',
+    11 => 'November',
+    12 => 'December'
+);
+
+$intervals = array(
+    1 => "the day",
+    2 => "the week",
+    3 => "the month",
+    4 => "the year"
+);
+
+$intsel = $intsel == '' ? '1' : $intsel;
+$yearsel = $yearsel == '' ? $year : $yearsel;
+$monthsel = $monthsel == '' ? $month : $monthsel;
+$daysel = $daysel == '' ? $day : $daysel;
+
+function dateref($interval, $year, $month, $day) {
+    return '<a href="' . "?page=graph&amp;interval=$interval&amp;year=$year&amp;month=$month&amp;day=$day" . '">';
+}
+
+function timeref($interval, $time) {
+    $year = date('Y', $time);
+    $month = date('n', $time);
+    $day = date('j', $time);
+
+    return dateref($interval, $year, $month, $day);
+}
+
+$starttime = mktime(0, 0, 0, $monthsel, $daysel, $yearsel);
+$endtime = $starttime + 86400;
+$cal_caption = date('l, F j Y', $starttime);
+
+if ($intsel == 2) {
+    $starttime = strtotime('this week', $starttime);
+    $endtime = $starttime + 86400 * 7;
+    $cal_caption = 'Week of ' . date('F j, Y', $starttime);
+} else if ($intsel == 3) {
+    $starttime = strtotime('first day of this month', $starttime);
+    $endtime = strtotime('first day of next month', $starttime);
+    $cal_caption = date('F Y', $starttime);
+} else if ($intsel == 4) {
+    $starttime = strtotime('first day of january', $starttime);
+    $endtime = strtotime('last day of december', $starttime);
+    $cal_caption = date('Y', $starttime);
+}
 ?>
--- donut.php
+++ donut.php
@@ -1,5 +1,6 @@
 <?php
-ini_set('display_errors', 1); 
+
+ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 include_once('./config.php');
@@ -11,58 +12,57 @@ $font = getcwd() . '/fonts/Roboto-Regular.ttf';
 $font_cond = getcwd() . '/fonts/RobotoCondensed-Regular.ttf';
 
 function fmtnumnum($bytes) {
-	$divisor = 1024;
-	$unit = "KB";
+    $divisor = 1024;
+    $unit = "KB";
 
-    while ($bytes / $divisor >= 1000.0) {		
-		$divisor *= 1024;
-	}
+    while ($bytes / $divisor >= 1000.0) {
+        $divisor *= 1024;
+    }
 
     $number = number_format($bytes / $divisor, 2, '.', '');
     return $number;
 }
 
 function fmtnumun($bytes) {
-	$divisor = 1024;
-	$unit = "KB";
+    $divisor = 1024;
+    $unit = "KB";
 
     while ($bytes / $divisor >= 1000.0) {
-		switch ($unit) {
-			case "KB":
-				$unit = "MB";
-				break;
-			case "MB":
-				$unit = "GB";
-				break;
-			case "GB":
-				$unit = "TB";
-				break;
-			default:
-				return;
-		}
-		
-		$divisor *= 1024;
-	}
+        switch ($unit) {
+            case "KB":
+                $unit = "MB";
+                break;
+            case "MB":
+                $unit = "GB";
+                break;
+            case "GB":
+                $unit = "TB";
+                break;
+            default:
+                return;
+        }
+
+        $divisor *= 1024;
+    }
 
     return $unit;
 }
 
 function fmtnum($bytes) {
-	return fmtnumnum($bytes) . ' ' . fmtnumun($bytes);
+    return fmtnumnum($bytes) . ' ' . fmtnumun($bytes);
 }
 
-
 if (@!($serial = $_GET['serial']))
-	exit;
+    exit;
 
 $sql = "select * from `$sim_table_name` " .
-	"where SimSerial='$serial'";
+    "where SimSerial='$serial'";
 
 if (!(@$resid = mysqli_query($connect, $sql)))
-	error('select', $connect);
+    error('select', $connect);
 
-if (!@$resarr = mysqli_fetch_assoc($resid)) 
-	exit;
+if (!@$resarr = mysqli_fetch_assoc($resid))
+    exit;
 
 mysqli_free_result($resid);
 
@@ -71,19 +71,19 @@ $simid = $resarr['SimId'];
 $used = $resarr['Current'] - $resarr['Floor'];
 
 if ($used < 0) {
-	$used = 0;
+    $used = 0;
 }
 
 $limit = $resarr['HasLimit'] ? $resarr['Limit'] : 0;
 
 if ($limit < 0) {
-	$limit = 0;
+    $limit = 0;
 }
 
 $percent = 0;
 
 if ($limit > 0 && $used > 0) {
-	$percent = $used / $limit;
+    $percent = $used / $limit;
 }
 
 $width = 800;
@@ -94,17 +94,17 @@ $smallfontsize = 20;
 
 $im = imagecreatetruecolor($width, $height);
 
-$transp = imagecolorallocate ($im, 0xFF, 0xFF, 0xFF);
+$transp = imagecolorallocate($im, 0xFF, 0xFF, 0xFF);
 imagecolortransparent($im, $transp);
 imagefilledrectangle($im, 0, 0, $width - 1, $height - 1, $transp);
 
 if (@$_GET['secret'] != $mdu_secret) {
-	imagepng($im);
-	imagedestroy($im);
-	exit;
+    imagepng($im);
+    imagedestroy($im);
+    exit;
 }
 
-$black = imagecolorallocate ($im, 0, 0, 0);
+$black = imagecolorallocate($im, 0, 0, 0);
 
 $green = imagecolorallocate($im, 0x6A, 0x9B, 0x93);
 $blue = imagecolorallocate($im, 0x3D, 0x63, 0x6F);
@@ -113,8 +113,8 @@ $grey = imagecolorallocate($im, 0xC0, 0xC0, 0xC0);
 imagefilledarc($im, 120, 120, 200, 200, 0, 360, $grey, IMG_ARC_PIE);
 
 if ($limit > 0) {
-	imagefilledarc($im, 120, 120, 200, 200, -90, -90 + $percent * 360, $green, 
-		IMG_ARC_PIE);
+    imagefilledarc($im, 120, 120, 200, 200, -90, -90 + round($percent * 360), $green,
+        IMG_ARC_PIE);
 }
 
 imagefilledarc($im, 120, 120, 90, 90, 0, 360, $transp, IMG_ARC_PIE);
@@ -141,14 +141,13 @@ $x = $ofsX - $textWidth - $textWidthU - 7;
 
 imagefttext($im, $ftsize, 0, $x, 218, $black, $font_cond, $text);
 
-
 $ftsize = 20;
 
 $text = 'used ' . floor($used / 1024 / 1024) . ' MB';
 
 if ($limit > 0) {
-	$text = floor(($limit - $used) / 1024 / 1024) . ' MB left, ' . $text;
-} 
+    $text = floor(($limit - $used) / 1024 / 1024) . ' MB left, ' . $text;
+}
 
 $dimensions = imagettfbbox($ftsize, 0, $font, $text);
 $textWidth = abs($dimensions[4] - $dimensions[0]);
@@ -157,9 +156,9 @@ $x = $ofsX - $textWidth;
 imagefttext($im, $ftsize, 0, $x, 320, $black, $font, $text);
 
 if ($limit > 0) {
-	$text = 'limit ' . floor($limit / 1024 / 1024) . ' MB';
+    $text = 'limit ' . floor($limit / 1024 / 1024) . ' MB';
 } else {
-	$text = 'no limit';
+    $text = 'no limit';
 }
 
 $dimensions = imagettfbbox($ftsize, 0, $font, $text);
@@ -168,60 +167,58 @@ $x = $ofsX - $textWidth;
 
 imagefttext($im, $ftsize, 0, $x, 365, $black, $font, $text);
 
-
 function drawcirc($x, $y, $bperc, $gperc, $text, $bnum, $gnum) {
-	global $im;
-	global $grey;
-	global $green;
-	global $blue;
-	global $black;
-	global $transp;
-	global $font;
-	
-	$s = 100;
-	$c = floor($s * 0.45);
-	
-	imagefilledarc($im, $x, $y, $s, $s, 0, 360, $grey, IMG_ARC_PIE);
+    global $im;
+    global $grey;
+    global $green;
+    global $blue;
+    global $black;
+    global $transp;
+    global $font;
 
-	if ($bperc > 0) {
-		imagefilledarc($im, $x, $y, $s, $s, -90, -90 + $bperc * 360, 
-			$blue, IMG_ARC_PIE);
-	}
-	
-	if ($gperc > 0) {
-		imagefilledarc($im, $x, $y, $s, $s, -90 + $bperc * 360, 
-			-90 + ($gperc + $bperc) * 360, 	$green, IMG_ARC_PIE);
-	}	
-		
-	imagefilledarc($im, $x, $y, $c, $c, 0, 360, $transp, IMG_ARC_PIE);
-	
-	imagefttext($im, 16, 0, $x - $s/2, $y + $s/2 + 28, $black, $font, $text);
-			
-	imagefilledrectangle($im, $x - $s/2, $y + $s/2 + 40,
-		$x - $s/2 + 10, $y + $s/2 + 40 + 10, $green);
-		
-	imagefttext($im, 12, 0, $x - $s/2 + 15, $y + $s/2 + 51, $black, $font, 
-		'RX = ' . fmtnum($gnum));
-		
-	imagefilledrectangle($im, $x - $s/2, $y + $s/2 + 60,
-		$x - $s/2 + 10, $y + $s/2 + 60 + 10, $blue);
-		
-	imagefttext($im, 12, 0, $x - $s/2 + 15, $y + $s/2 + 71, $black, $font, 
-		'TX = ' . fmtnum($bnum));
+    $s = 100;
+    $c = floor($s * 0.45);
 
+    imagefilledarc($im, $x, $y, $s, $s, 0, 360, $grey, IMG_ARC_PIE);
+
+    if ($bperc > 0) {
+        imagefilledarc($im, $x, $y, $s, $s, -90, -90 + round($bperc * 360),
+            $blue, IMG_ARC_PIE);
+    }
+
+    if ($gperc > 0) {
+        imagefilledarc($im, $x, $y, $s, $s, -90 + round($bperc * 360),
+            -90 + round(($gperc + $bperc) * 360), $green, IMG_ARC_PIE);
+    }
+
+    imagefilledarc($im, $x, $y, $c, $c, 0, 360, $transp, IMG_ARC_PIE);
+
+    imagefttext($im, 16, 0, $x - $s / 2, $y + $s / 2 + 28, $black, $font, $text);
+
+    imagefilledrectangle($im, $x - $s / 2, $y + $s / 2 + 40,
+        $x - $s / 2 + 10, $y + $s / 2 + 40 + 10, $green);
+
+    imagefttext($im, 12, 0, $x - $s / 2 + 15, $y + $s / 2 + 51, $black, $font,
+        'RX = ' . fmtnum($gnum));
+
+    imagefilledrectangle($im, $x - $s / 2, $y + $s / 2 + 60,
+        $x - $s / 2 + 10, $y + $s / 2 + 60 + 10, $blue);
+
+    imagefttext($im, 12, 0, $x - $s / 2 + 15, $y + $s / 2 + 71, $black, $font,
+        'TX = ' . fmtnum($bnum));
 }
 
 $starttime = mktime(0, 0, 0, $month, $day, $year) * 1000;
 
-$sql = "select sum(RxBytes) as Rx, sum(TxBytes) as Tx from $history_table_name" 
-	. " where SimId = $simid and StartTime >= $starttime and StartTime < " 
-	. ($starttime + 86400 * 1000);
+$sql = "select sum(RxBytes) as Rx, sum(TxBytes) as Tx from $history_table_name"
+    . " where SimId = $simid and StartTime >= $starttime and StartTime < "
+    . ($starttime + 86400 * 1000);
 
 if (!(@$resid = mysqli_query($connect, $sql)))
-	error('select', $connect);
+    error('select', $connect);
 
-if (!@$resarr = mysqli_fetch_assoc($resid)) 
-	exit;
+if (!@$resarr = mysqli_fetch_assoc($resid))
+    exit;
 
 mysqli_free_result($resid);
 
@@ -231,15 +228,15 @@ $today = $ttx + $trx;
 
 $starttime = $starttime - 86400 * 1000;
 
-$sql = "select sum(RxBytes) as Rx, sum(TxBytes) as Tx from $history_table_name" 
-	. " where SimId = $simid and StartTime >= $starttime and StartTime < " 
-	. ($starttime + 86400 * 1000);
+$sql = "select sum(RxBytes) as Rx, sum(TxBytes) as Tx from $history_table_name"
+    . " where SimId = $simid and StartTime >= $starttime and StartTime < "
+    . ($starttime + 86400 * 1000);
 
 if (!(@$resid = mysqli_query($connect, $sql)))
-	error('select', $connect);
+    error('select', $connect);
 
-if (!@$resarr = mysqli_fetch_assoc($resid)) 
-	exit;
+if (!@$resarr = mysqli_fetch_assoc($resid))
+    exit;
 
 mysqli_free_result($resid);
 
@@ -248,25 +245,25 @@ $yrx = $resarr['Rx'];
 $yesterday = $ytx + $yrx;
 
 if ($yesterday == 0) {
-	if ($today > 0) {
-		$trp = ($trx / $today);
-		$ttp = 1 - $trp;
-	} else {
-		$trp = 0;
-		$ttp = 0;
-	}
-	$yrp = 0;
-	$ytp = 0;
+    if ($today > 0) {
+        $trp = ($trx / $today);
+        $ttp = 1 - $trp;
+    } else {
+        $trp = 0;
+        $ttp = 0;
+    }
+    $yrp = 0;
+    $ytp = 0;
 } else if ($today < $yesterday) {
-	$trp = ($trx / $yesterday);
-	$ttp = ($ttx / $yesterday);
-	$yrp = ($yrx / $yesterday);
-	$ytp = 1 - $yrp;
+    $trp = ($trx / $yesterday);
+    $ttp = ($ttx / $yesterday);
+    $yrp = ($yrx / $yesterday);
+    $ytp = 1 - $yrp;
 } else {
-	$trp = ($trx / $today);
-	$ttp = 1 - $trp;
-	$yrp = ($yrx / $today);
-	$ytp = ($ytx / $today);
+    $trp = ($trx / $today);
+    $ttp = 1 - $trp;
+    $yrp = ($yrx / $today);
+    $ytp = ($ytx / $today);
 }
 
 drawcirc(550, 60, $ttp, $trp, "today", $ttx, $trx);
@@ -275,15 +272,15 @@ drawcirc(550, 255, $ytp, $yrp, "yesterday", $ytx, $yrx);
 $starttime = strtotime('first day of this month', $time) * 1000;
 $endtime = (strtotime('last day of this month', $time) + 86400) * 1000;
 
-$sql = "select sum(RxBytes) as Rx, sum(TxBytes) as Tx from $history_table_name" 
-	. " where SimId = $simid and StartTime >= $starttime and StartTime < " 
-	. $endtime;
+$sql = "select sum(RxBytes) as Rx, sum(TxBytes) as Tx from $history_table_name"
+    . " where SimId = $simid and StartTime >= $starttime and StartTime < "
+    . $endtime;
 
 if (!(@$resid = mysqli_query($connect, $sql)))
-	error('select', $connect);
+    error('select', $connect);
 
-if (!@$resarr = mysqli_fetch_assoc($resid)) 
-	exit;
+if (!@$resarr = mysqli_fetch_assoc($resid))
+    exit;
 
 mysqli_free_result($resid);
 
@@ -294,16 +291,16 @@ $today = $ttx + $trx;
 $starttime = strtotime('first day of last month', $time) * 1000;
 $endtime = (strtotime('last day of last month', $time) + 86400) * 1000;
 
-$sql = "select sum(RxBytes) as Rx, sum(TxBytes) as Tx from $history_table_name" 
-	. " where SimId = $simid and StartTime >= $starttime and StartTime < " 
-	. $endtime;
+$sql = "select sum(RxBytes) as Rx, sum(TxBytes) as Tx from $history_table_name"
+    . " where SimId = $simid and StartTime >= $starttime and StartTime < "
+    . $endtime;
 
 if (!(@$resid = mysqli_query($connect, $sql)))
-	error('select', $connect);
+    error('select', $connect);
 
-if (!@$resarr = mysqli_fetch_assoc($resid)) 
-	exit;
-	
+if (!@$resarr = mysqli_fetch_assoc($resid))
+    exit;
+
 mysqli_free_result($resid);
 
 $ytx = $resarr['Tx'];
@@ -311,25 +308,25 @@ $yrx = $resarr['Rx'];
 $yesterday = $ytx + $yrx;
 
 if ($yesterday == 0) {
-	if ($today > 0) {
-		$trp = ($trx / $today);
-		$ttp = 1 - $trp;
-	} else {
-		$trp = 0;
-		$ttp = 0;
-	}
-	$yrp = 0;
-	$ytp = 0;
+    if ($today > 0) {
+        $trp = ($trx / $today);
+        $ttp = 1 - $trp;
+    } else {
+        $trp = 0;
+        $ttp = 0;
+    }
+    $yrp = 0;
+    $ytp = 0;
 } else if ($today < $yesterday) {
-	$trp = ($trx / $yesterday);
-	$ttp = ($ttx / $yesterday);
-	$yrp = ($yrx / $yesterday);
-	$ytp = 1 - $yrp;
+    $trp = ($trx / $yesterday);
+    $ttp = ($ttx / $yesterday);
+    $yrp = ($yrx / $yesterday);
+    $ytp = 1 - $yrp;
 } else {
-	$trp = ($trx / $today);
-	$ttp = 1 - $trp;
-	$yrp = ($yrx / $today);
-	$ytp = ($ytx / $today);
+    $trp = ($trx / $today);
+    $ttp = 1 - $trp;
+    $yrp = ($yrx / $today);
+    $ytp = ($ytx / $today);
 }
 
 drawcirc(710, 60, $ttp, $trp, "this month", $ttx, $trx);
@@ -337,5 +334,4 @@ drawcirc(710, 255, $ytp, $yrp, "last month", $ytx, $yrx);
 
 imagepng($im);
 imagedestroy($im);
-
 ?>
